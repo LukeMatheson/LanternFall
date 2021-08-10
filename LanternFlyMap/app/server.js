a0745b8b1d1dcf4b76a4100193f053ab3a37febe
Richard <richardphong1@gmail.com>
2021-08-10 04:22:51 -0400

    Added server.js, env.json, and packages

---
+++ LanternFlyMap/app/server.js
@@ -0,0 +1,21 @@
+const pg = require("pg");
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const hostname = "localhost";
+
+const env = require("../env.json");
+const Pool = pg.Pool;
+const pool = new Pool(env);
+
+pool.connect().then(function () {
+    console.log(`Connected to database ${env.database}`);
+});
+
+app.use(express.static("public_html"));
+app.use(express.json());
+
+app.listen(port, hostname, () => {
+    console.log(`Listening at: http://${hostname}:${port}`);
+});
