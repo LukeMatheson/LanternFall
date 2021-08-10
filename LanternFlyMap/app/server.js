dbc86c22d6399ac8cec433276df4e2b4dd83d0aa
Richard <richardphong1@gmail.com>
2021-08-10 04:32:22 -0400

    Added bcrypt

--- LanternFlyMap/app/server.js
+++ LanternFlyMap/app/server.js
@@ -1,14 +1,20 @@
 const pg = require("pg");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const app = express();
 
 const port = 3000;
 const hostname = "localhost";
 
+// number of rounds the bcrypt algorithm will use to generate the salt
+// the more rounds, the longer it takes
+// so the salt will be more secure
+// https://github.com/kelektiv/node.bcrypt.js#a-note-on-rounds
+const saltRounds = 10;
+
 const env = require("../env.json");
 const Pool = pg.Pool;
 const pool = new Pool(env);
-
 pool.connect().then(function () {
     console.log(`Connected to database ${env.database}`);
 });
