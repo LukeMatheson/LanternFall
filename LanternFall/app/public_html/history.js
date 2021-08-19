9c7a9ea07eadf06056f77d4a39ce013c740055a2
cal352 <lijoichris@gmail.com>
2021-08-19 16:11:18 -0400

    Create profile boilerplate
    Created profile HTML boilerplate and edited history.js code

--- LanternFall/app/public_html/history.js
+++ LanternFall/app/public_html/history.js
@@ -9,17 +9,53 @@ function onclick() {
         desc: []
     };
     let err = document.getElementById("error-msg");
+    let date, kill, lat, long, desc;
 
     while (err.firstChild) {
         err.remove(err.firstChild);
     }
 
-    fetch("/history?user=" + username).then(function (response) {
+    fetch(`/history?username=${username}`).then(function (response) {
         code = response.status;
         return response.json();
     }).then(function (data) {
         if (code === 200) {
-            console.log(data);
+            for (let i = 0; i < data.info.date.length; i++) {
+                date = data.info.date[i];
+                kill = data.info.nickname[i];
+                lat = data.info.loc_lat[i];
+                long = data.info.loc_lon[i];
+                desc = data.info.description[i];
+    
+                let tr = document.createElement("tr");
+    
+                let td = document.createElement("td");
+                td.textContent = date;
+                td.classList.add("cell");
+                tr.append(td);
+    
+                td = document.createElement("td");
+                td.textContent = kill;
+                td.classList.add("cell");
+                tr.append(td);
+    
+                td = document.createElement("td");
+                td.textContent = lat;
+                td.classList.add("cell");
+                tr.append(td);
+    
+                td = document.createElement("td");
+                td.textContent = long;
+                td.classList.add("cell");
+                tr.append(td);
+    
+                td = document.createElement("td");
+                td.textContent = desc;
+                td.classList.add("cell");
+                tr.append(td);
+    
+                document.getElementById("profile-table").append(tr);
+            }
         } else {
             console.log(data.error);
             let div = document.createElement("div");
