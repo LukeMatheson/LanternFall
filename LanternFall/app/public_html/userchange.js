931a28ccbbf51026a620e8654c895a02468861a6
cal352 <lijoichris@gmail.com>
2021-08-22 10:43:41 -0400

    Update userchange.js
    Finished js file for the username change webpage, will update in server.js when available

--- LanternFall/app/public_html/userchange.js
+++ LanternFall/app/public_html/userchange.js
@@ -2,14 +2,39 @@ function onclick() {
     let newuser = document.getElementsByName("user-change")[0].value;
     let olduser = sessionStorage.getItem("username");
     let err = document.getElementById("error-msg");
+    let code = 200;
 
     while (err.firstChild) {
         err.remove(err.firstChild);
     }
 
-    //TODO: add fetch api call for a /userchange?olduser=${olduser}&newuser=${newuser} route
-    //TODO: add /userchange route to server.js file
+    let values = {
+        olduser: olduser,
+        newuser: newuser
+    };
+
+    fetch('/changeUsername', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+    }).then(function (response) {
+        code = response.status;
+        return response.json();
+    }).then(function (data) {
+        let div = document.createElement("div");
+        if (code === 200) {
+            div.textContent = data.success;
+            err.append(div);
+        } else {
+            div.textContent = data.error;
+            err.append(div);
+        }
+    });
 }
 
 let button = document.getElementById("submit-button");
 button.addEventListener("click", onclick);
+
+//NOTES: Should perform a query call in the server.js file, return a json body with the element success if successful or error if it is not.
