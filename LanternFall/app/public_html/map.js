7bcec470e69d3b5e988942516a9fd241c77440b2
John Dominguez <John.Dominguez101@gmail.com>
2021-09-02 19:09:55 -0400

    Pan button, bold on ham

--- LanternFall/app/public_html/map.js
+++ LanternFall/app/public_html/map.js
@@ -171,6 +171,7 @@ if (sessionStorage.getItem("username") != null) {
         const locationButton = document.createElement("button");
         locationButton.textContent = "Pan to Current Location";
         locationButton.classList.add("custom-map-control-button");
+        locationButton.classList.add("button-style");
         map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
         locationButton.addEventListener("click", () => {
             // Try HTML5 geolocation.
