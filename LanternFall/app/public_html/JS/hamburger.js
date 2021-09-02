c465ede3c165238d8da32cd6b7a7e9067cccd027
John Dominguez <John.Dominguez101@gmail.com>
2021-09-02 13:41:47 -0400

    More ham pages, map kill button fix

---
+++ LanternFall/app/public_html/JS/hamburger.js
@@ -0,0 +1,30 @@
+let tl = anime.timeline({
+    targets: ".ham-container",
+    easing: 'easeOutCirc',
+    duration: 300,
+    opacity: [0, 1],
+    width: '210px',
+    autoplay: false
+  })
+  .add({
+    delay: 100
+  })
+  .add({
+    targets: ".float-top, .float-bot",
+    color: "rgb(20 148 20)",
+    translateX: [-100, 0],
+    opacity: [0, 1],
+    delay: anime.stagger(100)
+  })
+  .add({
+      targets: "a",
+      color: "rgb(20 148 20)",
+      duration: 250
+  });
+
+  document.getElementById("btnHam").addEventListener("click", () => {
+    tl.play() ;
+    tl.finished.then(() => {
+        tl.reverse() ;
+    }) ;
+  });
